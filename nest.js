7f286cc97351569eafc5e30d1185028d70e820ac
stormboy <stormboy@stormboy.com>
2014-06-23 13:15:04 +1000

    fixed problem using else twice.

--- nest.js
+++ nest.js
@@ -180,14 +180,6 @@ NestMeem.prototype._subscribeNestDone = function(deviceId, data, type) {
 			self._fetchNestStatus();
 		}
 	}
-	else {
-		// no data, error from  nest call
-		var now = new Date().getTime();
-		if (now - self._lastUpdateTime > self._minStatusInterval) {
-			//console.log('Nest: no data');
-			self._fetchNestStatus();
-		}
-	}
 };
 
 NestMeem.prototype._connectMqtt = function() {
